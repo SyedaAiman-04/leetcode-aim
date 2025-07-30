d8c3848889ed93a5eade6ed33bc92a917053bf31
Syeda Aiman <syedahash04@gmail.com>
2025-07-30 19:45:10 +0530

    Time: 2 ms (72.29%), Space: 61.9 MB (29.98%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,15 @@
+class Solution{
+    static int maxProfit(int [] prices){
+        int min=prices[0];
+        int max=0;
+        for(int  i=1;i<prices.length;i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            else if(prices[i]-min>max){
+                max=prices[i]-min;
+            }
+        }
+        return max;
+    }
+}
